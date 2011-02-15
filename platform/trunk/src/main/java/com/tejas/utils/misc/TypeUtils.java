359f129aab3a8e577bebab5336d03a829ec478e5
alokgore <alokgore@10.14.111.45>
2011-02-16 00:07:43 +0530

    Fire and motion

--- platform/trunk/src/main/java/com/tejas/utils/misc/TypeUtils.java
+++ platform/trunk/src/main/java/com/tejas/utils/misc/TypeUtils.java
@@ -3,14 +3,30 @@ package com.tejas.utils.misc;
 public class TypeUtils
 {
     /**
-     * Null-safe equals 
+     * Null-safe equals
      */
     public static boolean equals(Object object1, Object object2)
     {
-            if(object1 == null && object2 == null) return true;
+        if ((object1 == null) && (object2 == null))
+        {
+            return true;
+        }
 
-            if(object1 == null || object2 == null) return false;
-            
-            return object1.equals(object2);
+        if ((object1 == null) || (object2 == null))
+        {
+            return false;
+        }
+
+        return object1.equals(object2);
+    }
+
+    public static boolean getBoolean(String boolString)
+    {
+        if (boolString != null)
+        {
+            boolString = boolString.trim().toUpperCase();
+            return boolString.startsWith("T") || boolString.startsWith("Y");
+        }
+        return false;
     }
 }
