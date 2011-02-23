cc877ec281c46214a57f280eaaeb14b8c71ca33e
Alok Gore <alokgore@Alok-Gores-MacBook-Pro.local>
2011-02-23 13:21:17 +0530

    Disabling Profiling in Tejas by default

--- platform/trunk/src/main/java/com/tejas/core/TejasResourceFactory.java
+++ platform/trunk/src/main/java/com/tejas/core/TejasResourceFactory.java
@@ -1,23 +1,29 @@
 package com.tejas.core;
 
 import com.tejas.alarming.Log4jAlarmHandler;
+import com.tejas.config.ApplicationConfig;
 import com.tejas.logging.TejasLog4jWrapper;
+import com.tejas.profiler.DummyMetricsHandler;
 import com.tejas.profiler.ProfilerMetricsHandler;
 
 public class TejasResourceFactory
 {
-	public static TejasLogger getLogger()
-	{
-		return TejasLog4jWrapper.getLogger(TejasResourceFactory.class);
-	}
-	
-	public static TejasEventHandler getAlarmHandler()
-	{
-		return new Log4jAlarmHandler();
-	}
+    public static TejasLogger getLogger()
+    {
+        return TejasLog4jWrapper.getLogger(TejasResourceFactory.class);
+    }
 
-	public static TejasMetricsHandler getMetricsHandler()
-	{
-		return new ProfilerMetricsHandler();
-	}
+    public static TejasEventHandler getAlarmHandler()
+    {
+        return new Log4jAlarmHandler();
+    }
+
+    public static TejasMetricsHandler getMetricsHandler()
+    {
+        if (ApplicationConfig.findBoolean("tejas.metrics.dummy", true))
+        {
+            return new DummyMetricsHandler();
+        }
+        return new ProfilerMetricsHandler();
+    }
 }
